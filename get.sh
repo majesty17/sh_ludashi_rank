42a41a70ee1b54d101af0604575a76947ed1aaf2
Majesty <a-o@qq.com>
2021-04-09 17:52:19 +0800

    Update get.sh

--- get.sh
+++ get.sh
@@ -1,3 +1,3 @@
-for i in `seq 355 368537`;do
-curl "http://www1.ludashi.com/index/hwrank?ver=1&point=$i&hwname=hahahaha&type=display" 2>/dev/null|grep "^                        <td class="
+for i in `seq 696560 -10 300`;do
+curl "http://www1.ludashi.com/index/hwrank?ver=1&point=$i&hwname=hahahaha&type=display"|grep "\"tab_lx\"\|\"tab_fs\"" 
 done
